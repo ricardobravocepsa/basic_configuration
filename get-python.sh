6fee10b34ac4ac88a1e0accdeecefa4a10b4a6ca
ricardobravocepsa <132470341+ricardobravocepsa@users.noreply.github.com>
2023-05-25 10:52:02 +0200

    Update get-python.sh

--- get-python.sh
+++ get-python.sh
@@ -5,10 +5,4 @@ sudo tar xzf Python-3.9.16.tgz
 cd Python-3.9.16 
 sudo ./configure --enable-optimizations 
 sudo make altinstall 
-echo "alias python='python3.9'" >> .bashrc
-echo "alias python3='python3.9'" >> .bashrc
-source .bashrc
-python3 -m venv venv 
-source venv/bin/activate 
-curl -sSL https://install.python-poetry.org | python3 -
 sudo rm -f /opt/Python-3.9.16.tgz 
