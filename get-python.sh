552139f1be4fb9c1b5512144fe319676a3fea9dc
ricardobravocepsa <132470341+ricardobravocepsa@users.noreply.github.com>
2023-07-08 09:09:20 +0200

    Update get-python.sh

--- get-python.sh
+++ get-python.sh
@@ -2,7 +2,7 @@
 cd /opt 
 sudo wget  https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz 
 sudo tar xzf Python-3.11.4.tgz 
-cd Python-3.9.16 
+cd Python-3.11.4
 sudo ./configure --enable-optimizations 
 sudo make altinstall 
 sudo rm -f /opt/Python-3.9.16.tgz 
